8f2fe40286e31aa917f35d10545a7f8af9de1e86
Reim6118 <issac5829@gmail.com>
2023-04-05 21:28:37 +0900

    SED update

--- SED.py
+++ SED.py
@@ -4,13 +4,16 @@ import pandas as pd
 import numpy as np
 import librosa as lb
 import soundfile as sf
+from more_itertools import chunked 
 from matplotlib import pyplot as plt
+
 
 #samplerate
 sr = 16000
 n_mels=32 
-n_fft=1024
+n_fft=128
 hop_length=512
+batch_size=4
 csvpath = r"C:\Users\issac\Documents\ML\Badminton_sound\sound.csv"
 audiopath = r"C:\Users\issac\Documents\ML\Badminton_sound\audio_wav"
 # def load(path):
@@ -27,14 +30,22 @@ def LoadCsv(path):
 
 def LoadAudio(path,Sr,mels,fft,hop_length):
     audio_files = lb.util.find_files(path)
+    print(audio_files)
+    Sdb_List= []
     for audio_file in audio_files:
         y, sr = sf.read(audio_file)
         Spec = lb.feature.melspectrogram(y=y, sr=Sr, n_mels=mels,n_fft=fft,hop_length=hop_length)
-        S_DB = lb.power_to_db(Spec, ref=np.max)
-    lb.display.specshow(S_DB, x_axis='time', y_axis='mel', sr=sr, fmax=8000)
-    plt.colorbar(format='%+2.0f dB')
-    plt.title('Mel Spectrogram')
-    plt.show()
+        Sdb = lb.power_to_db(Spec, ref=np.max)
+        print("SDB shape = ",Sdb.shape)
+    # y, sr = sf.read(path)
+    
+    # Spec = lb.feature.melspectrogram(y=y, sr=Sr, n_mels=mels,n_fft=fft,hop_length=hop_length)
+    # Sdb = lb.power_to_db(Spec, ref=np.max)
+    # print("SDB shape = ",Sdb.shape)
+    # lb.display.specshow(Sdb, x_axis='time', y_axis='mel', sr=sr, fmax=8000)
+    # plt.colorbar(format='%+2.0f dB')
+    # plt.title('Mel Spectrogram')
+    # plt.show()
     return
 
 
